9f711532a8c2a7b198a51d621f7ccd2c3b4cbf02
Abyou <emandaabyou@gmail.com>
2022-04-14 18:10:33 -0500

    A function has been implemented that retrieves weather reports from the Open Weather Map API and displays them dynamically in the user interface.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,19 @@ var key = "fe19897d62524787142ce36a5083a059";
 
 // Time format
 function day_format(date) {
-  const date1 = new Date();
-  console.log(date1);
-  const month = date.getMonth() + 1;
+  var date = new Date();
+  console.log(date);
+  var month = date.getMonth() + 1;
   var day = date.getDate();
 
-  const formatted_date =
-    date.getFullYear() + "/" + (month < 10 ? "0" : "") + month + s;
+  var formatted_date =
+    date.getFullYear() +
+    "-" +
+    (month < 10 ? "0" : "") +
+    month +
+    "-" +
+    (day < 10 ? "0" : "") +
+    day;
   "/" + (day < 10 ? "0" : "") + day;
   return formatted_date;
 }
@@ -48,7 +54,7 @@ function display_locations() {
   if (!location) {
     return;
   } else {
-    // get_weather_report(location);
+    fetch_weather_report(location);
   }
 }
 
@@ -69,6 +75,137 @@ $("#search_location_btn").on("click", function (event) {
   store_location();
 });
 
+function fetch_weather_report(location_name) {
+  var URL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location_name +
+    "&appid=" +
+    key;
+
+  $("#today_weather_report_container").empty();
+  $.ajax({
+    url: URL,
+    method: "GET",
+  }).then(function (response) {
+    location_title = $("<h3>").text(response.name);
+    $("#today_weather_report_container").append(location_title);
+
+    var get_tempeture_number = parseInt((response.main.temp * 9) / 5 - 459);
+    var location_temperature = $("<p>").text(
+      "Tempeture: " + get_tempeture_number + " °F"
+    );
+    $("#today_weather_report_container").append(location_temperature);
+    var location_humidity = $("<p>").text(
+      "Humidity: " + response.main.humidity + " %"
+    );
+    $("#today_weather_report_container").append(location_humidity);
+    var location_wind_speed = $("<p>").text(
+      "Wind Speed: " + response.wind.speed + " MPH"
+    );
+    $("#today_weather_report_container").append(location_wind_speed);
+    var coord_lon = response.coord.lon;
+    var coord_lat = response.coord.lat;
+
+    var RRL_UVI =
+      "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+      key +
+      "&lat=" +
+      coord_lat +
+      "&lon=" +
+      coord_lon;
+    $.ajax({
+      url: RRL_UVI,
+      method: "GET",
+    }).then(function (responseuv) {
+      var location_uv = $("<span>").text(responseuv.value);
+      var location_uvp = $("<p>").text("UV Index: ");
+      location_uvp.append(location_uv);
+      $("#today_weather_report_container").append(location_uvp);
+      console.log(typeof responseuv.value);
+    });
+    var url_forecast =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      location_name +
+      "&appid=" +
+      key;
+    $.ajax({
+      url: url_forecast,
+      method: "GET",
+    }).then(function (forecast_response) {
+      $("#weekly_list_container").empty();
+      console.log(forecast_response);
+      for (var index_1 = 0, index_2 = 0; index_2 <= 6; index_1 = index_1 + 7) {
+        var get_date = forecast_response.list[index_1].dt;
+        if (
+          forecast_response.list[index_1].dt !=
+          forecast_response.list[index_1 + 1].dt
+        ) {
+          var forcast_div = $("<div>");
+          forcast_div.attr("class", "report_container_box");
+          var d = new Date(0);
+          d.setUTCSeconds(get_date);
+          var date = d;
+          console.log(date);
+          var month = date.getMonth() + 1;
+          var day = date.getDate();
+          var date_format =
+            date.getFullYear() +
+            "-" +
+            (month < 10 ? "0" : "") +
+            month +
+            "-" +
+            (day < 10 ? "0" : "") +
+            day;
+          var forecast_response_h4 = $("<h6>").text(date_format);
+
+          var img_tag = $("<img>");
+          var weather_condition =
+            forecast_response.list[index_1].weather[0].main;
+          if (weather_condition === "Clouds") {
+            img_tag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/cloud.png"
+            );
+          } else if (weather_condition === "Clear") {
+            img_tag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/summer.png"
+            );
+          } else if (weather_condition === "Rain") {
+            img_tag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/rain.png"
+            );
+          }
+          var projected_temperature_k =
+            forecast_response.list[index_1].main.temp;
+          console.log(weather_condition);
+          var get_tempeture_number = parseInt(
+            (projected_temperature_k * 9) / 5 - 459
+          );
+          var projected_temperature = $("<p>").text(
+            "Tempeture: " + get_tempeture_number + " °F"
+          );
+          var projected_humidity = $("<p>").text(
+            "Humidity: " + forecast_response.list[index_1].main.humidity + " %"
+          );
+          forcast_div.append(forecast_response_h4);
+          forcast_div.append(img_tag);
+          forcast_div.append(projected_temperature);
+          forcast_div.append(projected_humidity);
+          $("#weekly_list_container").append(forcast_div);
+          console.log(forecast_response);
+          index_2++;
+        }
+      }
+    });
+  });
+}
+$(document).on("click", "#list_of_location", function () {
+  var thisLocation = $(this).attr("location_searched_data");
+  fetch_weather_report(thisLocation);
+});
+
 var currentDate =
   moment().format("dddd") + " " + moment().format("Do MMM YYYY");
 var presentHour = moment().format("h:mm:ss a");
@@ -76,12 +213,12 @@ var presentHour = moment().format("h:mm:ss a");
 // The date and the time
 var interval = setInterval(function () {
   var thisHour = moment();
-  $("#currentDay").html(
+  $("#todays_date").html(
     thisHour.format("YYYY MMMM DD") +
       " " +
       thisHour.format("dddd").substring(0, 3).toUpperCase()
   );
-  $("#currentDay").html(
+  $("#todays_date").html(
     currentDate + " " + thisHour.format("hh:mm:ss A") + " " + "Weather"
   );
 }, 100);
